8c6cf3336eeb5c1301a43d62095e65717d3ba714
Mayu <mayurccontato@gmail.com>
2025-01-26 14:14:16 -0300

    Atualizei o código da calculadora
    Adicionei uma função para as operações específicas e alterei algumas linhas anteriores.

--- Codigos/calculadora.py
+++ Codigos/calculadora.py
@@ -1,52 +1,94 @@
 from re import fullmatch
 from sympy import sympify
 from time import sleep
+import math
 
 class Calculadora:
 
     @staticmethod
-    def operar_basico():
+    def operar_simples():
         while True:
-            operacao = input('''Essa categoria permite:
-    - Adição (+)
-    - Subtração (-)
-    - Multiplicação (*)
-    - Divisão (/)
-    - Potência (**)
-    - Uso de colchetes [ ] e chaves { } para organização.
-Digite a operação ou "sair" para retornar ao menu.\n''')
+            operacao = input('Digite uma operação ou "sair" para retornar ao menu:\n')
 
             if operacao.lower() == 'sair':
                 Calculadora.menu()
                 break
-            elif fullmatch(r"[0-9.+\-*/()\[\]{} ]+", operacao):
+            elif fullmatch(r"[0-9.+\-*/()\[\]{}% ]+", operacao):
                 resultado = sympify(operacao)
-                print(f'O resultado é {resultado}')
+                print(f'RESULTADO: {resultado}')
                 sleep(1)
             else:
-                print('ERRO: Digite uma operação válida ou "sair".')
+                print('ERRO: Digite uma operação válida ou "sair":')
                 sleep(1)
+
+    @staticmethod
+    def operar_especifico():
+        while True:
+            selecao = input('''Selecione a operação:
+[1] Logaritmo
+[2] Fatoração
+Digite o número correspondente à opção desejada ou "sair" para retornar ao menu:\n''')
+
+            match selecao.lower():
+                case 'sair':
+                    Calculadora.menu()
+                    break
+                case '1':
+                    numero = input('Digite o número do qual você deseja o logaritmo:\n')
+                    base =  input('Digite a base do logaritmo:\n')
+                    if numero.isnumeric() and base.isnumeric():
+                        numero = int(numero)
+                        base = int(base)
+                        print(f'O logaritmo de {numero} na base {base} é {math.log(numero, base)}')
+                        sleep(1)
+                    else:
+                        print('ERRO: Digite valores válido ou "sair".')
+                        sleep(1)
+                case '2':
+                    numero = input('Digite o número do qual você deseja o fatorial.\n')
+                    if numero.isnumeric():
+                        numero = int(numero)
+                        print(f'O fatorial de {numero} é {math.factorial(numero)}')
+                        sleep(1)
+                    else:
+                        print('ERRO: Digite um número válido ou "sair".')
+                        sleep(1)
+                case _:
+                    print('ERRO: Digite uma opção válida ou "sair".')
+                    sleep(1)
 
     @staticmethod
     def menu():
         while True:
             selecao = input('''Os tipos de operação disponíveis são:
-[1] Operações Básicas
-[2] Operações Avançadas
+[1] Operações simples
+[2] Operações específicas
 [3] Conversões
 [4] Estatística
 [5] Geometria
 [6] Finanças
-Digite o número correspondente à opção desejada ou "sair" para encerrar o programa.\n''')
+Digite o número correspondente à opção desejada ou "sair" para encerrar o programa:\n''')
 
             match selecao.lower():
                 case 'sair':
                     exit()
                 case '1':
-                    Calculadora.operar_basico()
+                    print('''Essa categoria permite:
+    - Adição (+)
+    - Subtração (-)
+    - Multiplicação (*)
+    - Divisão comum (/)
+    - Divisão inteira (//)
+    - Resto da divisão (%)                   
+    - Potenciação (**)
+    - Radiciação (X ** (1/2) para raíz quadrada, X ** (1/3) para raíz cúbica e X ** (1/5) para raíz quinta)
+    - Porcentagem (X * (P/100) sendo P a porcentagem de X desejada)
+    - Uso de chaves, colchetes e parênteses''')
+                    Calculadora.operar_simples()
                     break
                 case '2':
-                    pass
+                    Calculadora.operar_especifico()
+                    break
                 case '3':
                     pass
                 case '4':
@@ -56,5 +98,5 @@ Digite o número correspondente à opção desejada ou "sair" para encerrar o pr
                 case '6':
                     pass
                 case _:
-                    print('ERRO: Digite uma opção válida ou "sair".')
+                    print('ERRO: Digite uma opção válida ou "sair":')
                     sleep(1)
